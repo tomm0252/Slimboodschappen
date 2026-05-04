5c03446e06c874e0ebaac60bd36c599835b6246c
tomm0252 <tomm18178@gmail.com>
2026-05-04 20:12:44 +0200

    Update prijzen.js

--- api/prijzen.js
+++ api/prijzen.js
@@ -1,9 +1,9 @@
 module.exports = async function handler(req, res) {
-res.setHeader(“Access-Control-Allow-Origin”, “*”);
+res.setHeader("Access-Control-Allow-Origin", "*");
 const { q } = req.query;
 return res.status(200).json({
-status: “werkt!”,
-query: q || “geen zoekterm”,
+status: "werkt!",
+query: q || "geen zoekterm",
 tijd: new Date().toISOString()
 });
 };
